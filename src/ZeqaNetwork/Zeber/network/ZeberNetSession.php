db3210f947f38e17106f16bf8b1cd8a4bfc9db68
AkmalFairuz <akmalf475@gmail.com>
2022-05-21 08:15:26 +0700

    TODO: Handle Packet

--- src/ZeqaNetwork/Zeber/network/ZeberNetSession.php
+++ src/ZeqaNetwork/Zeber/network/ZeberNetSession.php
@@ -8,6 +8,7 @@ use AkmalFairuz\Sobana\server\ServerSession;
 use pocketmine\utils\Utils;
 use ZeqaNetwork\Zeber\client\Client;
 use ZeqaNetwork\Zeber\client\ClientManager;
+use function igbinary_unserialize;
 use function json_encode;
 use function json_last_error_msg;
 
@@ -21,5 +22,13 @@ class ZeberNetSession extends ServerSession{
 
     public function sendPacket(array $data) {
         $this->write(Utils::assumeNotFalse(json_encode($data), "Failed to encode JSON: " . json_last_error_msg()));
+    }
+
+    public function handlePacket(string $packet): void{
+        $decoded = igbinary_unserialize($packet);
+    }
+
+    public function onClose(): void{
+        ClientManager::remove($this->client);
     }
 }
