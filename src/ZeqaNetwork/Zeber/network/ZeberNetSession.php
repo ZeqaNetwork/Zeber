500c511dfbd52b19392d59445042c371ef5e7406
AkmalFairuz <akmalf475@gmail.com>
2022-05-21 11:41:50 +0700

    Authentication

--- src/ZeqaNetwork/Zeber/network/ZeberNetSession.php
+++ src/ZeqaNetwork/Zeber/network/ZeberNetSession.php
@@ -36,7 +36,13 @@ class ZeberNetSession extends ServerSession{
                 switch($id){
                     case PacketId::LOGIN:
                         $loginInfo = LoginInfo::create($data);
-                        ClientManager::add($this->client = new Client($this, $this->getId(), $loginInfo->name, $loginInfo->type));
+                        $name = $loginInfo->name;
+                        if(ClientManager::getByName($name) !== null) {
+                            $this->sendPacket(PacketBuilder::create(PacketId::AUTH, false));
+                            $this->close();
+                            break;
+                        }
+                        ClientManager::add($this->client = new Client($this, $this->getId(), $name, $loginInfo->type));
                         $this->authenticated = true;
                         break;
                 }
